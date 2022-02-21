ed86ec55b7d68e310912f652b52f30b2a4929bdc
Chris Adams <chrisa@Chriss-Air.localdomain>
2022-02-20 16:54:07 -0800

    first commit

---
+++ config_template.js
@@ -0,0 +1,91 @@
+var config = {
+    devices: [
+        {id: 0, name: "hass.light.entityname", color: [0,0,0] }
+    ],
+    hass: {
+        host:"homeassistant.local:8123",
+        token: ""
+    },
+    xres: {
+        "state": {
+            "on": true,
+            "bri": 127,
+            "transition": 7,
+            "ps": -1,
+            "pl": -1,
+            "nl": {
+            "on": false,
+            "dur": 60,
+            "fade": true,
+            "tbri": 0
+        },
+        "udpn": {
+            "send": false,
+            "recv": true
+        },
+        // "seg": [{
+        //   "start": 0,
+        //   "stop": 1,
+        //   "len": 1,
+        //   "col": [
+        //     [255, 160, 0, 0],
+        //     [0, 0, 0, 0],
+        //     [0, 0, 0, 0]
+        //   ],
+        //   "fx": 0,
+        //   "sx": 127,
+        //   "ix": 127,
+        //   "pal": 0,
+        //   "sel": true,
+        //   "rev": false,
+        //   "cln": -1
+        // }]
+        },
+        "info": {
+            "ver": "0.8.4",
+            "vid": 1903252,
+            "leds": {
+            "count": 0,
+            "rgbw": false,
+            "pin": [2],
+            "pwr": 0,
+            "maxpwr": 65000,
+            "maxseg": 1
+        },
+        "name": "WLED Light",
+        "udpport": 19446,
+        "live": true,
+        "fxcount": 80,
+        "palcount": 47,
+        "arch": "esp8266",
+        "core": "2_4_2",
+        "freeheap": 13264,
+        "uptime": 17985,
+        "opt": 127,
+        "brand": "WLED",
+        "product": "DIY light",
+        "btype": "src",
+        "mac": "",
+        "ip": ""
+        },
+        "effects": [
+            "Solid", "Blink", "Breathe", "Wipe", "Wipe Random", "Random Colors", "Sweep", "Dynamic", "Colorloop", "Rainbow",
+            "Scan", "Dual Scan", "Fade", "Chase", "Chase Rainbow", "Running", "Saw", "Twinkle", "Dissolve", "Dissolve Rnd",
+            "Sparkle", "Dark Sparkle", "Sparkle+", "Strobe", "Strobe Rainbow", "Mega Strobe", "Blink Rainbow", "Android", "Chase", "Chase Random",
+            "Chase Rainbow", "Chase Flash", "Chase Flash Rnd", "Rainbow Runner", "Colorful", "Traffic Light", "Sweep Random", "Running 2", "Red & Blue","Stream",
+            "Scanner", "Lighthouse", "Fireworks", "Rain", "Merry Christmas", "Fire Flicker", "Gradient", "Loading", "In Out", "In In",
+            "Out Out", "Out In", "Circus", "Halloween", "Tri Chase", "Tri Wipe", "Tri Fade", "Lightning", "ICU", "Multi Comet",
+            "Dual Scanner", "Stream 2", "Oscillate", "Pride 2015", "Juggle", "Palette", "Fire 2012", "Colorwaves", "BPM", "Fill Noise", "Noise 1",
+            "Noise 2", "Noise 3", "Noise 4", "Colortwinkle", "Lake", "Meteor", "Smooth Meteor", "Railway", "Ripple"
+        ],
+            "palettes": [
+            "Default", "Random Cycle", "Primary Color", "Based on Primary", "Set Colors", "Based on Set", "Party", "Cloud", "Lava", "Ocean",
+            "Forest", "Rainbow", "Rainbow Bands", "Sunset", "Rivendell", "Breeze", "Red & Blue", "Yellowout", "Analogous", "Splash",
+            "Pastel", "Sunset 2", "Beech", "Vintage", "Departure", "Landscape", "Beach", "Sherbet", "Hult", "Hult 64",
+            "Drywet", "Jul", "Grintage", "Rewhi", "Tertiary", "Fire", "Icefire", "Cyane", "Light Pink", "Autumn",
+            "Magenta", "Magred", "Yelmag", "Yelblu", "Orange & Teal", "Tiamat", "April Night"
+        ]
+    }
+}
+
+module.exports = config;
---
+++ main.js
@@ -0,0 +1,140 @@
+var udp = require('dgram');
+var udpserver = udp.createSocket('udp4');
+
+var network = require('network');
+var config = require('./config.js');
+
+var express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
+var app = express();
+app.use(express.json());
+app.use('/', router);
+var restport = 80;
+
+var WebSocketClient = require('websocket').client;
+var wclient = new WebSocketClient();
+var globalconnection;
+var webs_ready = false;
+var webs_id = 0;
+
+config.xres.info.count = config.devices.length;
+network.get_interfaces_list((err, list) => {
+  var found = false;
+  if(!err) {
+    for(var i = 0; i < list.length; i++) {
+      var interface = list[i];
+      if(interface.status == 'active') {
+        config.xres.mac = interface.mac_address.replace(/:/g,'');
+        config.xres.ip = interface.ip_address;
+        console.log(`Using interface ${interface.name}, ${interface.ip_address}`);
+        found = true;
+        break;
+      }
+    }
+  }
+  if(!found) {
+    console.log("No active network interfaces found!");
+  }
+});
+
+arraysEqual = (arr1, arr2) => {
+  if(arr1.length == arr2.length) {
+    for(var i = 0; i < arr1.length; i++) {
+      if(arr1[i] != arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
+
+function getWebsCommand(color, light, id) {
+  return JSON.stringify({
+    "id": id,
+    "type": "call_service",
+    "domain": "light",
+    "service": "turn_on",
+    "service_data": {
+      "rgb_color": color
+    },
+    "target": {
+      "entity_id": light
+    }
+  });
+}
+
+//WLED JSON Webserver
+router.put('/json/state', (req, res) => {
+    console.log("Hyperion HTTP PUT Body:", JSON.stringify(req.body));
+    res.send(JSON.stringify(xres));
+});
+router.post('/json/state', (req, res) => {
+    console.log("Hyperion HTTP POST Body:", JSON.stringify(req.body));
+    res.send(JSON.stringify(xres));
+});
+app.listen(restport, () => {
+  console.log(`WLED JSON Server running on port ${restport}`);
+});
+
+//Hyperion "WLED" UDP Server
+udpserver.on("message", (msg, info) => {
+  if(webs_ready && config.devices.length <= msg.length / 3) {
+    var start = 0;
+    for(var d = 0; d < config.devices.length; d++) {
+      var color = [];
+      var end = start + 3;
+      for(i = start; i < end; i++) {
+        color.push(msg[i]);
+      }
+      if(!arraysEqual(devices[d].color, color)) {
+        webs_id++
+        config.devices[d].color = color;
+        //console.log(`Updating ID ${d}'s color, ID: ${webs_id}`);
+        var webscmd = getWebsCommand(config.devices[d].color, config.devices[d].name, webs_id);
+        globalconnection.send(webscmd);
+      }
+      start = end;
+    }
+  }
+});
+
+udpserver.on("listening", () => {
+  console.log("Hyperion UDP Server Listening...");
+});
+udpserver.on('error', (err) => {
+  console.log(`Hyperion UDP server error:\n${err.stack}`);
+  udpserver.close();
+});
+udpserver.bind(config.xres.udpport);
+
+//HASS Websocket Control
+wclient.on("connect", (connection) => {
+  console.log("HASS Websocket Connected.");
+  if(connection) {
+    connection.on("message", (msg) => {
+      var message = JSON.parse(msg.utf8Data);
+      var rmsg;
+      if(message.type == "auth_required") {
+        console.log("HASS Websocket Auth requested...");
+        rmsg = JSON.stringify({
+          "type": "auth",
+          "access_token": config.hass.token
+        });
+        connection.send(rmsg)
+      }
+      if(message.type == "auth_ok") {
+        console.log("HASS Websocket Auth Completed");
+        webs_ready = true;
+        globalconnection = connection;
+      }
+    });
+  }
+});
+
+wclient.on('connectFailed', function(error) {
+  console.log('Connect Error: ' + error.toString());
+});
+
+wclient.connect(`ws://${config.hass.host}/api/websocket`);
